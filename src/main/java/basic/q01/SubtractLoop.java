468978bf74c4e03716bb78bfbd9a2a15657c3b86
手島　大智 <d-tejima@seassist.co.jp>
2024-07-27 21:44:39 +0900

    add 1-1 q01~q07 first commit

---
+++ src/main/java/basic/q01/SubtractLoop.java
@@ -0,0 +1,14 @@
+package basic.q01;
+
+public class SubtractLoop {
+
+	public static void main(String[] args) {
+		int num = 101;
+
+		while (num > 0) {
+			num = num - 1;
+
+			System.out.println(num);
+		}
+	}
+}
